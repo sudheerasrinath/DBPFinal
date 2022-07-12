827307efe38f9460d8837062f23b6090ab7dbf19
sudheerasrinath <sudheerasrinath99@gmail.com>
2022-07-12 09:54:12 +0530

    createFXML

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class StudentFormController {
+    public JFXTextField txtNIC;
+    public JFXTextField txtEmail;
+    public JFXTextField txtAddress;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtContact;
+    public JFXTextField txtStudentId;
+    public TableColumn colNIC;
+    public TableColumn colAddress;
+    public TableColumn colContact;
+    public TableColumn colEmail;
+    public TableColumn colStudentName;
+    public TableColumn colStudentID;
+
+    public void btnAddStudentOnAction(ActionEvent actionEvent) {
+    }
+}
